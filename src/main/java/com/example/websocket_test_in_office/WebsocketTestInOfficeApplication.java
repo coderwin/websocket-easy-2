5aac164b2c0e7880cad576e9e29fd2a9b170421c
이호진 <lhj@hmit.co.kr>
2024-06-24 17:10:57 +0900

    init commit

---
+++ src/main/java/com/example/websocket_test_in_office/WebsocketTestInOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.example.websocket_test_in_office;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketTestInOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketTestInOfficeApplication.class, args);
+	}
+
+}
